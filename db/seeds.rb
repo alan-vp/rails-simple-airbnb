d1ef435e65e42536bcc9df405f1ce0b652b6d8bd
alan vergara <alan_vergara_p@hotmail.com>
2022-05-26 19:11:59 -0500

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cabin at the mountain in Liverpool',
+  address: 'West road 2023 Riverside Liverpool W103 98DR',
+  description: 'Log cabin with all the commodities, 3 rooms, a fireplace, full equiped kitchen',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Loft great for estudents',
+  address: '9026 Westwall Winchester N23 4DT',
+  description: 'Great location near of everything you could possibly ask for. Two double bedrooms, open plan living area, large kitchen and a beautiful living room',
+  price_per_night: 125,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Romantic Flat with a lake view',
+  address: '1094 Malta Gardens London W10 8DT',
+  description: 'One master bedroom with jacuzzi and sauna, perfect for a couple weekend',
+  price_per_night: 200,
+  number_of_guests: 2
+)
